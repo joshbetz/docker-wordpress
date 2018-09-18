c9b3d5574c6577be5b8eb5f0e3528c3b7e1ed3bc
Josh Betz <j@joshbetz.com>
2018-09-18 13:38:51 -0500

    Automatically build new images if there are changes

--- update.sh
+++ update.sh
@@ -13,13 +13,27 @@ sed \
 	-e "s/%%WORDPRESS_SHA1%%/${WORDPRESS_SHA1}/g" \
 	$DIR/Dockerfile-fpm.template > $DIR/fpm/Dockerfile
 
+# Build new image if there are changes
+if [[ ! $(git diff --quiet --exit-code $DIR/fpm/Dockerfile) ]]; then
+	git diff $DIR/fpm/Dockerfile
+
+	docker build -t joshbetz/wordpress $DIR/fpm
+fi
+
 ###
 # WP-CLI Dockerfile
 ###
-WPCLI_VERSION="$(curl https://api.github.com/repos/wp-cli/wp-cli/releases/latest | jq -r '.tag_name' | sed -e 's/^v//g' )"
+WPCLI_VERSION="$(curl -fsSL https://api.github.com/repos/wp-cli/wp-cli/releases/latest | jq -r '.tag_name' | sed -e 's/^v//g' )"
 WPCLI_SHA512="$(curl -fsSL "https://github.com/wp-cli/wp-cli/releases/download/v${WPCLI_VERSION}/wp-cli-${WPCLI_VERSION}.phar.sha512")"
 
 sed \
 	-e "s/%%WPCLI_VERSION%%/${WPCLI_VERSION}/g" \
 	-e "s/%%WPCLI_SHA512%%/${WPCLI_SHA512}/g" \
 	$DIR/Dockerfile-cli.template > $DIR/cli/Dockerfile
+
+# Build new image if there are changes
+if [[ ! $(git diff --quiet --exit-code $DIR/cli/Dockerfile) ]]; then
+	git diff $DIR/cli/Dockerfile
+
+	docker build -t joshbetz/wordpress:cli $DIR/cli
+fi
