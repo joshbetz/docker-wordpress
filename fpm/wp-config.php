279a1465c67e32e6858e32303b18bd38c48434f0
Josh Betz <j@joshbetz.com>
2018-11-06 11:46:47 -0600

    Add secret keys to wp-config

--- fpm/wp-config.php
+++ fpm/wp-config.php
@@ -9,6 +9,15 @@ define( 'DB_USER', $_ENV['WORDPRESS_DB_USER'] );
 define( 'DB_PASSWORD', $_ENV['WORDPRESS_DB_PASSWORD'] );
 define( 'DB_HOST', $_ENV['WORDPRESS_DB_HOST'] );
 
+define( 'AUTH_KEY', $_ENV['WORDPRESS_AUTH_KEY'] );
+define( 'SECURE_AUTH_KEY', $_ENV['WORDPRESS_SECURE_AUTH_KEY'] );
+define( 'LOGGED_IN_KEY', $_ENV['WORDPRESS_LOGGED_IN_KEY'] );
+define( 'NONCE_KEY', $_ENV['WORDPRESS_NONCE_KEY'] );
+define( 'AUTH_SALT', $_ENV['WORDPRESS_AUTH_SALT'] );
+define( 'SECURE_AUTH_SALT', $_ENV['WORDPRESS_SECURE_AUTH_SALT'] );
+define( 'LOGGED_IN_SALT', $_ENV['WORDPRESS_LOGGED_IN_SALT'] );
+define( 'NONCE_SALT', $_ENV['WORDPRESS_NONCE_SALT'] );
+
 $table_prefix = 'wp_';
 
 # content directory
