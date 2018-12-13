7054efa9ec4f413ec01cc9fad0caf0110136a6d6
Josh Betz <j@joshbetz.com>
2018-12-13 07:36:43 -0600

    Fix tests

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-for image in "$@"; do
+for dir in "$@"; do
+	image=$(echo $dir | sed -e "s/fpm/latest/")
 	if ! docker inspect "$image" &> /dev/null; then
 		echo "\timage does not exist!"
 		exit 1
