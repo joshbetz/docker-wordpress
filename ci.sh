bbc7dca9b871eeeb0236cfddfd01035679e16ebf
Josh Betz <j@joshbetz.com>
2024-07-05 20:59:39 -0500

    php 8.3

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 which jq > /dev/null || ( echo "Error: jq is required" && exit 1 )
 
-PHP_VERSION="$(curl -s 'https://www.php.net/releases/?json&version=8.2' | jq -r .version)"
+PHP_VERSION="$(curl -s 'https://www.php.net/releases/?json&version=8.3' | jq -r .version)"
 WORDPRESS_VERSION="$(curl -fsSL 'https://api.wordpress.org/core/version-check/1.7/' | jq -r '.offers[0].current')"
 WPCLI_VERSION="$(curl -fsSL https://api.github.com/repos/wp-cli/wp-cli/releases/latest | jq -r '.tag_name' | sed -e 's/^v//g' )"
 
