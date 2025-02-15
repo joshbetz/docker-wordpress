148f27fe76cc4799b627f056c0fbd63edb2ebb26
Josh Betz <j@joshbetz.com>
2025-02-15 15:46:43 -0600

    fix build scripts

--- ci.sh
+++ ci.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -eo pipefail
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 which jq > /dev/null || ( echo "Error: jq is required" && exit 1 )
@@ -10,7 +8,7 @@ PHP_VERSION="$(curl -s 'https://www.php.net/releases/?json&version=8.2' | jq -r 
 WORDPRESS_VERSION="$(curl -fsSL 'https://api.wordpress.org/core/version-check/1.7/' | jq -r '.offers[0].current')"
 WPCLI_VERSION="$(curl -fsSL https://api.github.com/repos/wp-cli/wp-cli/releases/latest | jq -r '.tag_name' | sed -e 's/^v//g' )"
 
-$DIR/update.sh "$PHP_VERSION" "$WORDPRESS_VERSION" "$WPCLI_VERSION" push
+$DIR/update.sh "$PHP_VERSION" "$WORDPRESS_VERSION" "$WPCLI_VERSION" push || (echo "Failed to update" && exit 1)
 
 git commit $DIR/fpm -m "WordPress $WORDPRESS_VERSION"
 
--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PHP_VERSION=$1
